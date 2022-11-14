a8ac12411f49bb88e1868097bc70bb77fe54c8ce
InnaLia <lyaschevainna@gmail.com>
2022-11-13 17:13:13 -0800

    ManipulationsWithArrays.java,ManipulationsWithArraysTest.java

--- src/main/java/ManipulationsWithArrays.java
+++ src/main/java/ManipulationsWithArrays.java
@@ -1,8 +1,8 @@
 public class ManipulationsWithArrays{
 
-    // Написать метод multiplуArrayByNumber(), который принимает на вход массив целых чисел и число int number.
+    // Написать метод multiplyArrayByNumber(), который принимает на вход массив целых чисел и число int number.
     // Метод возвращает массив тех же чисел, умноженных на number
-    //Например, multiplуArrayByNumber()({1, 2, 3, 4, 5}, 3) -> {3, 6, 9, 12, 15}
+    //Например, multiplyArrayByNumber()({1, 2, 3, 4, 5}, 3) -> {3, 6, 9, 12, 15}
 
     public int[] multiplyArrayByNumber (int[] array, int number) {
         int[] product = new int[array.length];
@@ -12,4 +12,41 @@ public class ManipulationsWithArrays{
         }
         return product;
     }
+    //Написать метод toDoubleArray(), который принимает на вход массив целых чисел,  и возвращает массив типа
+    // double[] из тех же чисел
+    //Например, toDoubleArray({1, 2, 3, 4, 5}) -> {1.0, 2.0, 3.0, 4.0, 5.0}
+    public double[] toDoubleArray(int[] array){
+
+        double[] result = new double[array.length];
+        for (int i = 0; i < array.length; i++){
+            result[i] = array[i];
+        }
+
+        return result;
+    }
+    //Написать метод toIntArray(), который принимает на вход массив типа double[],  и возвращает массив типа int[]
+    // из тех же чисел
+    //Например, toIntArray({1.1, 2.5, 3.7, 4.0, 5.5}) -> {1, 2, 3, 4, 5}
+    public int[] toIntArray(double[] array){
+
+        int[] result = new int[array.length];
+        for( int i = 0; i < array.length; i++){
+            result[i] = (int) array[i];
+        }
+        return result;
+    }
+
+
+    //Написать метод toStringArray(), который принимает на вход массив целых чисел,  и возвращает массив типа String[]
+    // из тех же чисел (желательно не использовать метод .toString(), нужно переводить вручную)
+    //Например, toStringArray({1, 2, 3, 4, 5}) -> {“1”, “2”, “3”, “4”, “5”}
+    public String[] toStringArray( int[] array) {
+
+        String[] result = new String[array.length];
+        for( int i = 0; i < array.length; i++){
+            result[i] = "" + array[i];
+        }
+        return result;
+    }
 }
+
--- src/test/java/ManipulationsWithArraysTest.java
+++ src/test/java/ManipulationsWithArraysTest.java
@@ -1,6 +1,8 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 public class ManipulationsWithArraysTest {
     // Написать метод multiplуArrayByNumber(), который принимает на вход массив целых чисел и число int number.
     // Метод возвращает массив тех же чисел, умноженных на number
@@ -35,5 +37,47 @@ public class ManipulationsWithArraysTest {
         int[] ActualResult = new ManipulationsWithArrays().multiplyArrayByNumber(array, number);
         Assert.assertEquals(ActualResult, ExpectedResult);
     }
+    //Написать метод toDoubleArray(), который принимает на вход массив целых чисел,  и возвращает массив типа
+    // double[] из тех же чисел
+    //Например, toDoubleArray({1, 2, 3, 4, 5}) -> {1.0, 2.0, 3.0, 4.0, 5.0}
+    @Test
+    public void testToDoubleArray(){
+        int[] array = {1, 2, 3, 4, 5};
+
+        double[] ExpectedResult = {1.0, 2.0, 3.0, 4.0, 5.0};
+        double[] ActualResult = new ManipulationsWithArrays().toDoubleArray(array);
+        Assert.assertEquals(ActualResult, ExpectedResult);
+        System.out.println(Arrays.toString(ExpectedResult));
+        System.out.println(Arrays.toString(ActualResult));
+    }
+    //Написать метод toIntArray(), который принимает на вход массив типа double[],  и возвращает массив типа int[]
+    // из тех же чисел
+    //Например, toIntArray({1.1, 2.5, 3.7, 4.0, 5.5}) -> {1, 2, 3, 4, 5}
+    @Test
+    public void testToIntArray(){
+        double[] array = {1.1, 2.5, 3.7, 4.0, 5.5};
+
+        int[] ExpectedResult = {1, 2, 3, 4, 5};
+        int[] ActualResult = new ManipulationsWithArrays().toIntArray(array);
+        Assert.assertEquals(ActualResult, ExpectedResult);
+        System.out.println(Arrays.toString(ExpectedResult));
+        System.out.println(Arrays.toString(ActualResult));
+    }
+    //Написать метод toStringArray(), который принимает на вход массив целых чисел,  и возвращает массив типа String[]
+    // из тех же чисел (желательно не использовать метод .toString(), нужно переводить вручную)
+    //Например, toStringArray({1, 2, 3, 4, 5}) -> {“1”, “2”, “3”, “4”, “5”}
+    @Test
+    public void testToStringArray(){
+        int[] array = {1, 2, 3, 4, 5};
+
+        String[] ExpectedResult = {"1", "2", "3", "4", "5"};
+        String[] ActualResult = new ManipulationsWithArrays().toStringArray(array);
+        Assert.assertEquals(ActualResult, ExpectedResult);
+        System.out.println(ActualResult);
+        System.out.println(ExpectedResult);
+    }
+
+
+
 
 }
